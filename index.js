5d54fb1df0e889bc9545f8c8e68abcc54f46cc48
mustymaz <mustyxmazhar@gmail.com>
2025-02-17 21:18:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["$500,000 Program", "🔗 JOIN US!"];
+const statusMessages = ["$500,000 Program", "🔗 JOIN TODAY!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
